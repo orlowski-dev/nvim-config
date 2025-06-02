9ac32583be3fc2608973e496952c616770963f4f
Patryk Orłowski <orlowski.dev@gmail.com>
2025-06-02 18:40:20 +0200

    enabled aura-theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -19,4 +19,15 @@
 -- 		vim.cmd.colorscheme("catppuccin")
 -- 	end,
 -- }
-return {}
+return {
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{
+		"baliestri/aura-theme",
+		lazy = false,
+		priority = 1000,
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+			vim.cmd([[colorscheme aura-dark]])
+		end,
+	},
+}
