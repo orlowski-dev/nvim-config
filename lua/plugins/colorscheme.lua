e6f931f5bb9abe9d93dcb2e32f7ba65fcfdb5882
Patryk Orłowski <orlowski.dev@gmail.com>
2025-06-04 05:36:15 +0200

    Changed theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,8 @@
--- return {
--- 	"nyoom-engineering/oxocarbon.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {},
---
--- 	config = function()
--- 		vim.opt.background = "dark" -- set this to dark or light
--- 		vim.cmd.colorscheme("oxocarbon")
--- 	end,
--- }
---
-
--- return {
--- 	"catppuccin/nvim",
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd.colorscheme("catppuccin")
--- 	end,
--- }
 return {
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{
-		"baliestri/aura-theme",
-		lazy = false,
-		priority = 1000,
-		config = function(plugin)
-			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-			vim.cmd([[colorscheme aura-dark]])
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
